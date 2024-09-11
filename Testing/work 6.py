a26792c639f1b60394d9d2b2ccb78ba0ae34f6be
Arub bin Ali <arubbinali@outlook.com>
2024-09-11 17:12:04 +0300

    `

---
+++ Testing/work 6.py
@@ -0,0 +1,12 @@
+        <a href="https://twitter.com/yourprofile" target="_blank" class="button" id="twitter-button">
+            <img src="path-to-your-twitter-image.png" alt="Twitter">
+        </a>
+        <a href="https://linkedin.com/in/yourprofile" target="_blank" class="button" id="linkedin-button">
+            <img src="path-to-your-linkedin-image.png" alt="LinkedIn">
+        </a>
+        <a href="https://facebook.com/yourprofile" target="_blank" class="button" id="facebook-button">
+            <img src="path-to-your-facebook-image.png" alt="Facebook">
+        </a>
+        <a href="https://instagram.com/yourprofile" target="_blank" class="button" id="instagram-button">
+            <img src="path-to-your-instagram-image.png" alt="Instagram">
+        </a>
